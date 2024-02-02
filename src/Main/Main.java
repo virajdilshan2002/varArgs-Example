04e48af9083e74dafc15a44184480e914fe7eefe
viraj dilshan <virajdilshan2019@gmail.com>
2024-02-02 21:44:38 +0530

    Var Args

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,19 +1,13 @@
 package Main;
 
-import otherClasses.*;
-
+import java.util.*;
 public class Main {
+    public static void printData(int... data){
+        System.out.println(Arrays.toString(data));
+    }
     public static void main(String[] args) {
-        varArgs e1 = new varArgs();
-        e1.printData();
-
-        varArgs v1 = new varArgs(50,"Hello!",100);
-        v1.printData();
-
-        varArgs<Integer> s1 = new varArgs("Hello!",50,200);
-        s1.printData();
-
-        varArgs<String> s2 = new varArgs<>("Hello!","50","Viraj");
-        s2.printData();
+        printData(10,20);
+        printData(100,200,300);
+        printData(11,22,33,44,55);
     }
 }
