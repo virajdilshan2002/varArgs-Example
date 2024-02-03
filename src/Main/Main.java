d48b488f960b0e99d381ffcd94437d16faec0de8
viraj dilshan <virajdilshan2019@gmail.com>
2024-02-03 10:26:26 +0530

    variable argument

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,13 @@
 package Main;
 
 import java.util.*;
+
 public class Main {
+
     public static void printData(int... data){
         System.out.println(Arrays.toString(data));
     }
+
     public static void main(String[] args) {
         printData(10,20);
         printData(100,200,300);
